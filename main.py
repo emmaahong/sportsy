5eeaf431d6e017470f5cb78f591e5544729f76f9
emmaahong <emmahong06@gmail.com>
2023-03-30 09:43:55 -0400

    preliminary nav bar and separate html pages - currently, they do NOT work

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+  
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+  
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
     
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port=5000)
