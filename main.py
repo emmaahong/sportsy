e87c5fbecfedbb0d98fad36052846ce5e72aff6b
emmaahong <emmahong06@gmail.com>
2023-03-30 10:41:38 -0400

    working url routing with multiple html pages

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
   
@@ -14,6 +14,10 @@ def profile():
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
+
+@app.route('/roster')
+def roster():
+    return render_template('roster.html')
     
 if __name__ == "__main__":
   app.run(debug=True, port=5000)
